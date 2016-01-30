c2d4fc52f76f30f0f400ba415c62b78fc21ea2fb
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-01-30 10:43:33 -0300

    change implementation to cpp file

--- frontier.cpp
+++ frontier.cpp
@@ -1,5 +1,29 @@
 #include "frontier.h"
 
-Frontier::Frontier()
+AI_Search::Frontier::Frontier(State* initialState)
 {
+    _states.push_back(initialState);
+}
+virtual AI_Search::Frontier::~Frontier()
+{
+    _states.clear();
+}
+
+void AI_Search::Frontier::addStates(std::vector<State*> states, std::function<bool(State*, State*)> sortAlgorithm)
+{
+    _states.insert(_states.end(), states.begin(), states.end());
+    std::sort(_states.begin(), _states.end(), sortAlgorithm);
+}
+void AI_Search::Frontier::removeFirst()
+{
+    _states.erase(_states.begin());
+}
+void AI_Search::Frontier::removeAt(unsigned int index)
+{
+    _states.erase(_states.begin() + index);
+}
+
+std::vector<AI_Search::State*> AI_Search::Frontier::getStates() const
+{
+    return _states;
 }
--- frontier.h
+++ frontier.h
@@ -10,33 +10,13 @@ class Frontier
 {
     std::vector<State*> _states;
 public:
-    Frontier(State* initialState)
-    {
-        _states.push_back(initialState);
-    }
-    virtual ~Frontier()
-    {
-        _states.clear();
-    }
+    Frontier(State* initialState);
+    virtual ~Frontier();
 
-    void addStates(std::vector<State*> states, std::function<bool(State*, State*)> sortAlgorithm)
-    {
-        _states.insert(_states.end(), states.begin(), states.end());
-        std::sort(_states.begin(), _states.end(), sortAlgorithm);
-    }
-    void removeFirst()
-    {
-        _states.erase(_states.begin());
-    }
-    void removeAt(unsigned int index)
-    {
-        _states.erase(_states.begin() + index);
-    }
-
-    std::vector<State*> getStates() const
-    {
-        return _states;
-    }
+    void addStates(std::vector<State*> states, std::function<bool(State*, State*)> sortAlgorithm);
+    void removeFirst();
+    void removeAt(unsigned int index);
+    std::vector<State*> getStates() const;
 };
 
 }
